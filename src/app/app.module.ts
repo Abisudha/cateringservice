d6c3bc1e3daf03054ffd3ecda8100d7340d321ad
Abisudha <abisudha1997@gmail.com>
2023-11-20 09:22:57 +0530

    Initial  commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderComponent } from './order/order.component';
 
 
 @NgModule({
@@ -19,7 +21,9 @@ import { HomeComponent } from './home/home.component';
     SignUpComponent,
     AboutUsComponent,
     ContactUsComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent {
+collapseAllTabs(arg0: string) {
+throw new Error('Method not implemented.');
+}
+
+}
+
+
+
+        // var openedBtn = "";
+        
+        // function collapseAllTabs(clickedBtnId){
+
+            // if(openedBtn != "")
+                // document.getElementById(openedBtn).click();
+            // openedBtn = clickedBtnId;
+        // }
+    
